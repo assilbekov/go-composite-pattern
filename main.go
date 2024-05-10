db3cf085b6deb31f7a02c143ee4856876a69fc54
assilbekov <askhat.assilbekov@gmail.com>
2024-05-10 18:45:52 -0300

    implement search by motherboard

--- main.go
+++ main.go
@@ -1,3 +1,30 @@
 package main
 
-func main() {}
+import "composite/pkg"
+
+var (
+	cpu = pkg.CPU{
+		Name:        "Intel",
+		Description: "Intel CPU",
+	}
+	gpu = pkg.GPU{
+		Name:        "NVIDIA",
+		Description: "NVIDIA GPU",
+	}
+	motherBoard = pkg.MotherBoard{
+		Name:        "Asus",
+		Description: "Asus MotherBoard",
+		Components: []pkg.Component{
+			&cpu,
+			&gpu,
+		},
+	}
+)
+
+func main() {
+	motherBoard.Search("Intel")
+	motherBoard.Search("NVIDIA")
+	motherBoard.Search("Asus")
+	motherBoard.Search("AMD")
+	motherBoard.Search("AMD")
+}
